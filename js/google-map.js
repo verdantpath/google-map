7f4dca6812aca5bc1c8469f1af2ecc778bfd9ca9
Matt <m_dittmann@yahoo.com>
2017-07-11 23:20:31 -0700

    update google-map.js

--- js/google-map.js
+++ js/google-map.js
@@ -9,6 +9,7 @@ function init() {
 	var venueMap;
 	venueMap = new google.maps.Map(document.getElementById('map'), mapOptions);
 }
+// create a script element, load the Google Maps API, call init(), append script to body
 function loadScript() {
 	var script = document.createElement('script');
 	script.src = 'http://maps.googleapis.com/maps/api/js?sensor+false&callback=init';
